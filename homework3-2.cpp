f12dde200c72645307af3b68b7d8659f6dc0d1d8
Verjulo <usted0709@gmail.com>
2026-03-12 11:56:43 +0800

    功課3-2完成

---
+++ homework3-2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void homework3_2()
+{
+    int a = 48; // 0011 0000
+    int b = 51; // 0011 0011
+    a = a << 2; // 1100 0000
+    b = b >> 3; // 0000 0011
+    printf("%d\n", a);
+    printf("%d\n", b);
+    b = ~b;
+    printf("%d\n", b);
+}
+int main()
+{
+    float y;
+    int a = 6, b = 4, c = 2;
+    int x = 3;
+    y = (float)(((a - 5) / 7) * x * x * x) + ((b + 9) / 5 * x * x) + ((c - 2) / 3 * x + 1);
+    printf("%f\n", y);
+    homework3_2();
+}
